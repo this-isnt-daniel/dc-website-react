7defc7ac0618c1ccd311aa2a950c6f436783f7bb
daniel <daniel.adrian.victor@gmail.com>
2025-09-10 20:38:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+      Hello World!
+
+    </div>
+  );
+}
+
+export default App;
