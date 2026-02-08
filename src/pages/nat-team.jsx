8d1df1851d677cfaeba94bb928e9dc350750547f
daniel <daniel.adrian.victor@gmail.com>
2026-02-09 01:23:22 +0530

    added nat team images and page hero

--- src/pages/nat-team.jsx
+++ src/pages/nat-team.jsx
@@ -1,6 +1,11 @@
-function NationalTeamPage (){
-    return(
-        <div />
+function NationalTeamPage() {
+    return (
+        <div className="national-team-page">
+            <div className="national-team-hero">
+                <h1>The National Debating <br /> Team of Sri Lanka</h1>
+            </div>
+            {/* Content will go here */}
+        </div>
     )
 }
 export default NationalTeamPage;
